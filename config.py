df7b90164990a7da1a1b1075755f71db80a943e0
brian tran <btran@hamilton.edu>
2026-01-29 12:33:16 -0800

    config files are safe

---
+++ config.py
@@ -0,0 +1,13 @@
+url = "http://nghttp2.org"
+# url = "https://nghttp2.org"
+# url = "https://www.catan.com"
+silent = False
+
+# Chaos
+loss_in = 0.12
+loss_out = 0.32
+
+# Not needed to adjust, because running config will update these. Required for running run.py.
+host = "nghttp2.org"
+port = 80
+scheme = "http"
